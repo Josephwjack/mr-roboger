151ed4bf61bff4534c8029a5e36f7cd2e11e7c5b
Josephwjack <joseph.w.jack@gmail.com>
2022-06-10 16:57:53 -0700

    add wrongtype test

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,21 @@ function robotNumbers(number) {
   for(let index=0; index <= number; index++) {
     if (index.toString().includes(3)) {
       numArray.push("wont you be my neighbor")
+
     }else if(index.toString().includes(2)) {
       numArray.push("boop")
+
     }else if(index.toString().includes(1)) {
       numArray.push("beep")
+
     }else {
       numArray.push(index);
     }
-  } return numArray
+  } return numArray;
 }
 
-function wrongType() {
-  if (Element === !Number){
+function wrongType(element) {
+  if (element === !Number){
     return alert("Please enter a real number")
   }
 };
